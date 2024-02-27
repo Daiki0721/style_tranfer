9ab0f235a013d75453822cf9b82e42fea2dd3e01
daiki <daiki139045@gmail.com>
2024-02-27 18:57:16 +0900

    o

--- picture_upload/views.py
+++ picture_upload/views.py
@@ -1,10 +1,11 @@
-
+import tensorflow as tf
 
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from .models import UploadImage, Styles
 from django.views import generic
 from .forms import UploadImageForm
+from model import generate_images
 
 class UploadImageCreateView(generic.CreateView):
     model = UploadImage
